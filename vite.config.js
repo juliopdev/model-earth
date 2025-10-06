e224c26e32998e4d1243f974d22112b790ffd462
JPX-0 <juliopariona2018@gmail.com>
2025-10-06 00:21:52 -0500

    v1.1

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
-const repoName = '[model-earth]'; 
+const repoName = 'model-earth'; 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
